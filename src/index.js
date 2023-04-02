bfe9636b9ee6629d5014609d01b98cc86284525f
Izahn <izahnpaulmiru1@gmail.com>
2023-04-02 21:24:35 +1200

    added something

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-const fullName = 'Izahn Paul-Miru'
-console.log(fullname)
+// let x = 0 
+// label: for (let i = 0; i < 5; i++){
+//     console.log("i =", i)
+//     inner: for (let j = 0; j < 5; j++){
+//         console.log("j =", j)
+//         x++; // x = x(0) + 1 
+//         console.log("x =", x)
+//         if (i == 2) break label; 
+//     }
+// }
+// console.log("sum =", x)
 
-var address = '28 Sovereign St'
-console.log(address)
-
-let age = 17
-console.log(age)
+let func = function() {} // anoonymous function/ a function with no name
+console.log(typeof func)
