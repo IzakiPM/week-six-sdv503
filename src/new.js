e94945dcb1eba5a3ff7122cbab1f79fe5ab76ec7
Izahn <izahnpaulmiru1@gmail.com>
2023-04-02 21:29:30 +1200

    Just revisiting activities

---
+++ src/new.js
@@ -0,0 +1,11 @@
+const fullName = "Henry"
+console.log(fullName)
+
+const middleName = "Jones"
+console.log(middleName)
+
+const lastName = "Chaplin"
+console.log(lastName)
+
+let address = "23 Justin Lane"
+console.log(address)
